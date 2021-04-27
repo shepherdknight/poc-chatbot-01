5f65ebf6b00288eeb5fc04421ff7ebf89d458ac6
shepherdknight <81963137+shepherdknight@users.noreply.github.com>
2021-04-27 20:25:34 +0800

    master

--- src/App.js
+++ src/App.js
@@ -7,16 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        React App - Demo AWS Chat Widget
+        Demo - AWS Chat Widget
         </p>
-        <a
-          className="App-link"
-          href="https://www.sykes.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          POC Chat Widget
-        </a>
+
       </header>
     </div>
   );
